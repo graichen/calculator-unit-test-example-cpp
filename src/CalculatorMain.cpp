9478531e59682d4bbffb5f79b48430a843a8f68d
Cathy Graichen <graichen@ge.com>
2020-11-29 16:55:33 -0500

    fix windows environment variable syntax, format of printf for only ints

--- src/CalculatorMain.cpp
+++ src/CalculatorMain.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char **argv) {
     Calculator calculator;
-    printf("1 + 1 = %f\n", calculator.sum(1, 1));
-    printf("4 - 2 = %f\n", calculator.minus(4, 2));
-    printf("2.5 * 1.5 = %f\n", calculator.multiply(2.5, 1.5));
-    printf("10 / 2 = %f\n", calculator.divide(10, 2));
+    printf("1 + 1 = %d\n", calculator.sum(1, 1));
+    printf("4 - 2 = %d\n", calculator.minus(4, 2));
+    printf("25 * 15 = %d\n", calculator.multiply(25, 15));
+    printf("10 / 2 = %d\n", calculator.divide(10, 2));
 }
